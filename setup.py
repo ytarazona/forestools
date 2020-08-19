5b14c1f81f2f085fc825995544d48766c48fba1f
Yonatan Tarazona Coronel <geoyons@gmail.com>
2020-08-19 15:26:48 -0500

    setup

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "License :: OSI Approved :: MIT License",
         'Natural Language :: English',
         "Operating System :: OS Independent",
