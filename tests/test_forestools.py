7aa5390cd3be9d9dd7f6af45b5f71576bd4b1e27
Yonatan Tarazona Coronel <geoyons@gmail.com>
2021-12-03 23:58:59 -0500

    Update test_forestools.py

--- tests/test_forestools.py
+++ tests/test_forestools.py
@@ -4,11 +4,8 @@
 
 
 import unittest
-from click.testing import CliRunner
 
 from forestools import forestools
-from forestools import cli
-
 
 class TestForestools(unittest.TestCase):
     """Tests for `forestools` package."""
@@ -21,13 +18,3 @@ class TestForestools(unittest.TestCase):
 
     def test_000_something(self):
         """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'forestools.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
