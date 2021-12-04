81ab68bef38af1ce9b724e0adb4cd265a70e57e2
Yonatan Tarazona Coronel <geoyons@gmail.com>
2021-12-03 23:47:14 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,40 +1,56 @@
 #!/usr/bin/env python
-# coding: utf-8
+"""The setup script."""
 
-# In[ ]:
-
-
+import io
+from os import path as op
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+here = op.abspath(op.dirname(__file__))
+
+# get the dependencies and installs
+with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
+
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
+
+requirements = ['Click>=7.0', ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
 
 setup(
-    name="forestools",
-    version="0.1.3",
-    author="Yonatan Tarazona Coronel",
-    author_email="geoyons@gmail.com",
-    description="Tools for detecting deforestation and forest degradation",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ytarazona/forestools",
-    install_requires=[
-      'numpy',
-      'pandas',
-      'matplotlib',
-      'datetime'
-    ],
-    packages=find_packages(),
+    author="Yonatan Tarazona",
+    author_email='geoyons@gmail.com',
+    python_requires='>=3.5',
+    version="0.1.4",
     classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        "License :: OSI Approved :: MIT License",
-        'Natural Language :: English',
-        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    description="Tools for detecting deforestation and forest degradation",
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='forestools',
+    name='forestools',
+    packages=find_packages(include=['forestools', 'forestools.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/ytarazona/forestools',
+    zip_safe=False,
 )
-
